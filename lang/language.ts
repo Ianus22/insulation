ce33da2aa9bee073fcc3c076ff77996774e61bad
Spasimir Pavlov <spasimir.pavlov@gmail.com>
2024-07-09 11:40:15 +0200

    fixed some bugs

--- lang/language.ts
+++ lang/language.ts
@@ -2,9 +2,7 @@ import { createLocalization } from '@/hooks/localization';
 import { en } from './en';
 import { de } from './de';
 
-const [languageState, useLocalization] = createLocalization({ en, de }, (localStorage.getItem('lang') as any) ?? 'en');
-
-languageState.listeners.add(() => localStorage.setItem('lang', languageState.value));
+const [languageState, useLocalization] = createLocalization({ en, de }, 'en');
 
 export { languageState, useLocalization };
 
