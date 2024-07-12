1d9e74b6d4f8d307009f5b2dfc8a85db388bf464
nik5an <krysteffsm@gmail.com>
2024-07-12 11:09:10 +0200

    fixed typo

--- lang/de.ts
+++ lang/de.ts
@@ -6,136 +6,141 @@ const de: Translations = {
   // ---------------------------------   PAGES   ---------------------------------
 
   // --------------------------------- Main page ---------------------------------
-  moto: "Erhalten Sie KI-Gesteuerte Isolierungsvorschläge",
-  sub_moto: "Automatisch und Schnell",
+  moto: 'Erhalten Sie KI-Gesteuerte Isolierungsvorschläge',
+  sub_moto: 'Automatisch und Schnell',
 
-  btn_TryItOut: "Versuch es!",
+  btn_TryItOut: 'Versuch es!',
 
-  lb_SeeItClearly: "Es klar sehen!",
-  lb_ImgInputExample: "Beispielbildeingabe",
+  lb_SeeItClearly: 'Es klar sehen!',
+  lb_ImgInputExample: 'Beispielbildeingabe',
 
-  txt_AdditionalPromtExample: "Ich benötige Vorschläge zur Verbesserung der Thermik und Schalldämmung",
-  lb_AdditionalPromt: "Beispiel für eine zusätzliche Eingabeaufforderung",
+  txt_AdditionalPromtExample: 'Ich benötige Vorschläge zur Verbesserung der Thermik und Schalldämmung',
+  lb_AdditionalPromt: 'Beispiel für eine zusätzliche Eingabeaufforderung',
 
-  txt_Output: "Sie sollten zwischen den Metallpfosten eine Isolierung aus Glasfasermatten anbringen, um die thermische Effizienz zu verbessern Schalldämmung...",
-  lb_Output: "Ausgabe",
+  txt_Output:
+    'Sie sollten zwischen den Metallpfosten eine Isolierung aus Glasfasermatten anbringen, um die thermische Effizienz zu verbessern Schalldämmung...',
+  lb_Output: 'Ausgabe',
 
-  txt_ContentDescription: "Laden Sie Ihr Wandbild hoch und erhalten Sie KI-gestützte Empfehlungen für die besten Dämmmaterialien zur Verbesserung thermische Effizienz und Schalldämmung.",
+  txt_ContentDescription:
+    'Laden Sie Ihr Wandbild hoch und erhalten Sie KI-gestützte Empfehlungen für die besten Dämmmaterialien zur Verbesserung thermische Effizienz und Schalldämmung.',
 
-   //  --------------------------------- Tool page ---------------------------------
-   lb_tl_Title: "Chats",
-   btn_tl_Close: "Schließen",
-   btn_tl_NewChat: "Neuer Chat",
- 
-   lb_tlF_ImageUpload: "Ziehen Sie es per Drag & Drop oder klicken Sie hier, um ein Bild hochzuladen",
-   lb_tlF_Prompt: "Zusätzliche Aufforderung",
- 
-   lb_tlF_RemoveImage: "Entferne Bild",
-   btn_tlF_Submit: "Einreichen",
-  
+  //  --------------------------------- Tool page ---------------------------------
+  lb_tl_Title: 'Chats',
+  btn_tl_Close: 'Schließen',
+  btn_tl_NewChat: 'Neuer Chat',
+
+  lb_tlF_ImageUpload: 'Ziehen Sie es per Drag & Drop oder klicken Sie hier, um ein Bild hochzuladen',
+  lb_tlF_Prompt: 'Zusätzliche Aufforderung',
+
+  lb_tlF_RemoveImage: 'Entferne Bild',
+  btn_tlF_Submit: 'Einreichen',
+
   // --------------------------------- How To Use page ---------------------------------
-  lb_htu_Title: "Anleitung!",
-  lb_htu_Description: "Auf dieser Seite finden Sie eine einfache Anleitung, wie Sie unsere KI nutzen, um Ihre Ziele effizient zu erreichen. Befolgen Sie die folgenden Schritte, um zu beginnen:",
+  lb_htu_Title: 'Anleitung!',
+  lb_htu_Description:
+    'Auf dieser Seite finden Sie eine einfache Anleitung, wie Sie unsere KI nutzen, um Ihre Ziele effizient zu erreichen. Befolgen Sie die folgenden Schritte, um zu beginnen:',
 
-  lb_htu_step1_Title: "Schritt 1: Bild hochladen",
-  lb_htu_step1_Description: "Machen Sie zunächst ein klares Foto des Gebäudes oder der Wand, die Sie isolieren möchten. Anschließend laden Sie das Bild ganz einfach mit unserer Drag-and-Drop- oder Upload-Funktion hoch.",
+  lb_htu_step1_Title: 'Schritt 1: Bild hochladen',
+  lb_htu_step1_Description:
+    'Machen Sie zunächst ein klares Foto des Gebäudes oder der Wand, die Sie isolieren möchten. Anschließend laden Sie das Bild ganz einfach mit unserer Drag-and-Drop- oder Upload-Funktion hoch.',
 
-  lb_htu_step2_Title: "Schritt 2: KI-Analyse",
-  lb_htu_step2_Description: "Jetzt beginnen wir mit dem Prozess. Unser fortschrittliches KI-Modell analysiert das Bild, um die beste Isolierungslösung zu ermitteln, die auf Ihre spezifischen Anforderungen zugeschnitten ist. Seien Sie versichert, wir sind bestrebt, Ihnen die effektivsten und effizientesten Ergebnisse zu liefern.",
+  lb_htu_step2_Title: 'Schritt 2: KI-Analyse',
+  lb_htu_step2_Description:
+    'Jetzt beginnen wir mit dem Prozess. Unser fortschrittliches KI-Modell analysiert das Bild, um die beste Isolierungslösung zu ermitteln, die auf Ihre spezifischen Anforderungen zugeschnitten ist. Seien Sie versichert, wir sind bestrebt, Ihnen die effektivsten und effizientesten Ergebnisse zu liefern.',
 
-  lb_htu_step3_Title: "Schritt 3: Lösung erhalten",
-  lb_htu_step3_Description: "Sobald unsere Arbeit abgeschlossen ist, erhalten Sie eine umfassende Lösung aus unserem fortschrittlichen KI-Modell mit detaillierten Angaben zu den optimalen Isolierungsmöglichkeiten und Installationsempfehlungen. Wir wissen Ihr Vertrauen in unsere Dienstleistungen zu schätzen und freuen uns darauf, außergewöhnliche Ergebnisse für mehr Energieeffizienz und Komfort zu liefern.",
+  lb_htu_step3_Title: 'Schritt 3: Lösung erhalten',
+  lb_htu_step3_Description:
+    'Sobald unsere Arbeit abgeschlossen ist, erhalten Sie eine umfassende Lösung aus unserem fortschrittlichen KI-Modell mit detaillierten Angaben zu den optimalen Isolierungsmöglichkeiten und Installationsempfehlungen. Wir wissen Ihr Vertrauen in unsere Dienstleistungen zu schätzen und freuen uns darauf, außergewöhnliche Ergebnisse für mehr Energieeffizienz und Komfort zu liefern.',
 
   // --------------------------------- Subscriptions page -------------------------------
-  lb_Subscriptions: "Abonnements",
+  lb_Subscriptions: 'Abonnements',
 
-  dlbx_SubscriptionConformation: "Sind Sie sicher, dass Sie Ihr Abonnement kündigen möchten?",
+  dlbx_SubscriptionConformation: 'Sind Sie sicher, dass Sie Ihr Abonnement kündigen möchten?',
 
-  dlbx_ConfirmSubscitptionOption: "Schließen",
-  dlbx_CancelSubscrioptionOption: "Stornieren",
+  dlbx_ConfirmSubscitptionOption: 'Schließen',
+  dlbx_CancelSubscrioptionOption: 'Stornieren',
 
-  lb_sb_SelectButton: "Wählen",
+  lb_sb_SelectButton: 'Wählen',
 
-  lb_sb1_Title: "Kostenlose Testphase*",
-  lb_sb1_Price: "€0",
-  lb_sb1_Duration: "/Monat",
-  lb_sb1_FirstChar: "3 kostenlose Testversionen",
-  lb_sb1_SecondChar: "begrenztes Ergebnis",
+  lb_sb1_Title: 'Kostenlose Testphase*',
+  lb_sb1_Price: '€0',
+  lb_sb1_Duration: '/Monat',
+  lb_sb1_FirstChar: '3 kostenlose Testversionen',
+  lb_sb1_SecondChar: 'begrenztes Ergebnis',
 
-  lb_sb2_Title: "Monatliches Abo",
-  lb_sb2_Price: "€14.99",
-  lb_sb2_Duration: "/Monat",
+  lb_sb2_Title: 'Monatliches Abo',
+  lb_sb2_Price: '€14.99',
+  lb_sb2_Duration: '/Monat',
 
-  lb_sb3_Title: "Jahresabonnement",
-  lb_sb3_Price: "€99",
-  lb_sb3_Duration: "/Jahr",
+  lb_sb3_Title: 'Jahresabonnement',
+  lb_sb3_Price: '€99',
+  lb_sb3_Duration: '/Jahr',
 
-  lb_sb2_sb3_FirstChar: "Unbegrenzte Gespräche",
-  lb_sb2_sb3_SecondChar: "Genaue und vollständige Antworten",
-  lb_sb2_sb3_ThirdChar: "Chatverlauf",
-  lb_sb2_sb3_FourthChar: "Fortsetzung alter Chats",
+  lb_sb2_sb3_FirstChar: 'Unbegrenzte Gespräche',
+  lb_sb2_sb3_SecondChar: 'Genaue und vollständige Antworten',
+  lb_sb2_sb3_ThirdChar: 'Chatverlauf',
+  lb_sb2_sb3_FourthChar: 'Fortsetzung alter Chats',
 
   //  --------------------------------- Contacts page ---------------------------------
-  lb_ct_Title: "In Kontakt kommen",
-  
-  lb_ct_Adress: "Rienößlgasse 3, Wien",
-  lb_ct_Telephone: "+359 58 331 24",
+  lb_ct_Title: 'In Kontakt kommen',
 
-  lb_ctF_Title: "Hinterlassen Sie uns eine Nachricht",
-  lb_ctF_NamePlaceholder: "Name",
-  lb_ctF_EmailPlaceholder: "Email",
-  lb_ctF_MessagePlaceholder: "Schreiben Sie uns hier eine Nachricht",
+  lb_ct_Adress: 'Rienößlgasse 3, Wien',
+  lb_ct_Telephone: '+359 58 331 24',
 
-  lb_ctF_SubmitButton: "Einreichen",
+  lb_ctF_Title: 'Hinterlassen Sie uns eine Nachricht',
+  lb_ctF_NamePlaceholder: 'Name',
+  lb_ctF_EmailPlaceholder: 'Email',
+  lb_ctF_MessagePlaceholder: 'Schreiben Sie uns hier eine Nachricht',
+
+  lb_ctF_SubmitButton: 'Einreichen',
 
   //  --------------------------------- Sign Up page ---------------------------------
-  lb_sgu_Title: "Melden Sie sich an",
+  lb_sgu_Title: 'Melden Sie sich an',
 
-  lb_sgu_sgi_F_Email: "E-Mail-Adresse",
-  lb_sgu_sgi_F_Password: "Passwort",
+  lb_sgu_sgi_F_Email: 'E-Mail-Adresse',
+  lb_sgu_sgi_F_Password: 'Passwort',
 
-  lb_sguF_PasswordRepeat: "Passwort erneut", 
+  lb_sguF_PasswordRepeat: 'Passwort erneut',
 
-  btn_sgu_SignUpButton: "Melden Sie sich an",
+  btn_sgu_SignUpButton: 'Melden Sie sich an',
 
-  lb_sgu_HaveAnAccount: "Sie haben bereits ein Konto?",
-  lb_sgu_Ahref_LogIn: "Anmeldung",
+  lb_sgu_HaveAnAccount: 'Sie haben bereits ein Konto?',
+  lb_sgu_Ahref_LogIn: 'Anmeldung',
 
   //  --------------------------------- Sign In page ---------------------------------
-  lb_sgi_Title: "Melden Sie sich bei Ihrem Konto an",
-  btn_sgu_SignInButton: "Anmelden",
+  lb_sgi_Title: 'Melden Sie sich bei Ihrem Konto an',
+  btn_sgu_SignInButton: 'Anmelden',
 
-  lb_sgi_Ahref_ForgotPassword: "Passwort vergessen?",
-  lb_sgi_NotAMember: "Kein Mitglied?",
+  lb_sgi_Ahref_ForgotPassword: 'Passwort vergessen?',
+  lb_sgi_NotAMember: 'Kein Mitglied?',
 
-  lb_sgi_Ahref_SignUp: "Melden Sie sich an",
+  lb_sgi_Ahref_SignUp: 'Melden Sie sich an',
 
   // --------------------------------- COMPONENTS -----------------------------
 
   // --------------------------------- Navbar ---------------------------------
-  lb_nvb_WebLogo: "SmartInsulation",
-  btn_nvb_HowToUse: "Wie benutzt man",
-  btn_nvb_Subscriptions: "Abonnements",
-  btn_nvb_Contacts: "Kontakte",
-  btn_nvb_SignIn: "Anmelden",
-  btn_nvb_Logout: "Abmelden",
+  lb_nvb_WebLogo: 'SmartInsulation',
+  btn_nvb_HowToUse: 'Wie es funktioniert',
+  btn_nvb_Subscriptions: 'Abonnements',
+  btn_nvb_Contacts: 'Kontakte',
+  btn_nvb_SignIn: 'Anmelden',
+  btn_nvb_Logout: 'Abmelden',
 
-  dlbxTitle_LogoutConfirmation: "Bist du sicher?",
-  dlbx_LogoutConfirmation: "Sind Sie sicher, dass Sie sich abmelden möchten",
-  dlbx_ConfirmOption: "Abmelden",
-  dlbx_CancelOption: "Stornieren",
+  dlbxTitle_LogoutConfirmation: 'Bist du sicher?',
+  dlbx_LogoutConfirmation: 'Sind Sie sicher, dass Sie sich abmelden möchten',
+  dlbx_ConfirmOption: 'Abmelden',
+  dlbx_CancelOption: 'Stornieren',
 
   // --------------------------------- Footer ---------------------------------
-  lb_ft_WebLogo: "2024 SmartInsulation",
+  lb_ft_WebLogo: '2024 SmartInsulation',
 
-  btn_ft_HowToUse: "Anleitung",
-  btn_ft_Donations: "Spenden",
-  btn_ft_Subscriptions: "Abonnements",
-  btn_ft_Contacts: "Kontakte",
+  btn_ft_HowToUse: 'Anleitung',
+  btn_ft_Donations: 'Spenden',
+  btn_ft_Subscriptions: 'Abonnements',
+  btn_ft_Contacts: 'Kontakte',
 
   // --------------------------------- Spinner ---------------------------------
-  lb_sp_Title: "Wird geladen..."
+  lb_sp_Title: 'Wird geladen...'
 };
 
 export { de };
-
